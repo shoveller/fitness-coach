4d78d26c9e8e3bf0a3351244b8e9224e4dd5e0ac
서재원 <cinos81@gmail.com>
2026-09-11 14:56:14 +0900

    feat: 리셋 기능을 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import type {SubmitEventHandler} from "react";
 
 function App() {
   const agent = useAgent({ agent: 'ThinkAgent' })
-  const { messages, sendMessage } = useAgentChat({ agent })
+  const { messages, sendMessage, clearHistory } = useAgentChat({ agent })
 
   const onSubmit: SubmitEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
@@ -24,6 +24,7 @@ function App() {
       <form onSubmit={onSubmit}>
         <input name="input" />
         <button type="submit">보내기</button>
+        <button type="reset" onClick={clearHistory}>리셋</button>
       </form>
       <div>{JSON.stringify(messages)}</div>
     </>
